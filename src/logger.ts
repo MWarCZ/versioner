3536b16cca78b934f3c608eea0c1aa29e3a7251e
MWarCZ <mv.deoxys@gmail.com>
2019-08-24 17:08:00 +0200

    Vytvoreni obalu pro vypisovani na konzoli.
     # Co:
    - Vytvoren obal pro vypisovani textu na konzoli.
      - Automaticke nastaveni urovne logu dle env `NODE_ENV`.
      - `NODE_ENV=DEBUG` Vypisuji se i debug zpravy.
     # Proc:
    - Moznost snadno oddelit typy vypisu.
      - DEBUG vypisy jen v DEBUG verzi.
      - Snazsi pozdejsi implementace ticheho rezimu.
    - Moznost pro budouci zmenu cile vystupu.
      - Z konzole napr. do souboru.

---
+++ src/logger.ts
@@ -0,0 +1,23 @@
+// Project: versioner
+// File: src/logger.ts
+import jslogger from 'js-logger'
+
+/**
+ * @example
+ * logger.trace('trace')
+ * logger.debug('debug')
+ * logger.info('info')
+ * logger.warn('warn')
+ * logger.error('error')
+ */
+const logger = jslogger
+
+logger.useDefaults()
+
+if (process.env.NODE_ENV === 'DEBUG') {
+  logger.setLevel(logger.DEBUG)
+} else {
+  logger.setLevel(logger.INFO)
+}
+
+export default logger
